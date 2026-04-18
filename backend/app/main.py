c1ed5890999499afc0635ab4b0c5411c2e353b54
El Mehdi <mehdi.elbrahmi@proton.me>
2026-04-18 02:09:32 +0100

    update

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,7 +24,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.ALLOWED_ORIGINS,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 app.add_middleware(SecurityHeadersMiddleware)
 app.add_middleware(ProxyHeadersMiddleware, trusted_hosts="*")
